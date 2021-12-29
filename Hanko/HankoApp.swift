baef5d965928fc020007e0ad31e58f196704c6b4
Lukas Romsicki <lukas@romsicki.com>
2021-12-28 21:01:15 -0500

    Initial Commit

---
+++ Hanko/HankoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HankoApp.swift
+//  Hanko
+//
+//  Created by Lukas Romsicki on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct HankoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
